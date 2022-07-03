4c214b5fe162a4c3ef5e004b19ab60043134b2af
ManoJesus <lucas_748l@hotmail.com>
2022-07-03 15:58:17 -0300

    first commit with initializing of angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'course-manager';
+  title:string = 'course-manager';
+
+  mainUser:User = {
+    id :1,
+    name : "Lucas",
+    email : "lucas@email.com",
+    password : "123Pass"
+  }  
+  
+}
+
+
+interface User {
+  id:number;
+  name:string;
+  email:string;
+  password:string;
+}
+
+function sum(a:number, b:number) {
+  return a + b;
 }
