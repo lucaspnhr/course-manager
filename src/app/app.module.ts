aeee20911d383706466897ac5fd3ff9199d58a2d
ManoJesus <lucas_748l@hotmail.com>
2022-07-04 10:38:06 -0300

    creating component and initialising project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './components/course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
